3f14f0781884a5e2680c39323b2b311d7b6580e7
stefan-lfc14 <stefan.ilic314@gmail.com>
2021-11-13 15:39:24 -0600

    /push

--- firstViz.js
+++ firstViz.js
@@ -1,1 +1,16 @@
-console.log("Hello! ")
+
+Let width = 970
+Let height = 1000
+
+var svg = d3.select("#viz")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("width", width)
+        .style("height", height)
+
+svg.append("circle")
+    .attr("cx", 50)
+    .attr("cy", 50)
+    .attr("r",40)
+    .attr("fill","lavender")
