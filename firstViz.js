b62f5725403d6228eb6ef9ee0678c82c700d148f
stefan-lfc14 <stefan.ilic314@gmail.com>
2021-11-13 15:47:54 -0600

    /push

--- firstViz.js
+++ firstViz.js
@@ -1,6 +1,6 @@
 
-Let width = 970;
-Let height = 1000;
+let width = 970;
+let height = 1000;
 
 var svg = d3.select("#viz")
         .append("svg")
@@ -14,3 +14,10 @@ svg.append("circle")
     .attr("cy", 50)
     .attr("r",40)
     .attr("fill","lavender")
+
+svg.append("rect")
+    .attr("x", 120)
+    .attr("y", 10)
+    .attr("width", 200)
+    .attr("height", 80)
+    .attr("fill","lightblue")
